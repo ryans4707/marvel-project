9872af0fd961c1ba4572d3fe3203b08ffdebb8ff
ryans4707 <ryans4707@hstat.org>
2017-03-10 19:21:57 +0000

    add files

---
+++ application_controller.rb
@@ -0,0 +1,19 @@
+require_relative 'models/model.rb'
+
+class ApplicationController < Sinatra::Base
+
+  get '/' do
+    erb :index
+  end
+  
+  post '/result' do
+    user_name = params[:name]
+    user_playlist_id = params[:playlist_id]
+    @user_playlist = Playlist.new(user_name,user_playlist_id)
+    @user_playlist.get_info
+    @user_playlist.get_lyrics
+    
+    erb :result
+  end
+
+end
